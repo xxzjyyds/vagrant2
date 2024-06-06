441b9d39080f8b63e054b211bd0148fd147bd84c
Jason <74402861+xxzjyyds@users.noreply.github.com>
2024-06-07 03:48:23 +0800

    Update VagrantFile_env.sh

--- vagrantfile/VagrantFile_env.sh
+++ vagrantfile/VagrantFile_env.sh
@@ -16,7 +16,7 @@ MYSQL_IP_ADDRESS=${MYSQL_IP_ADDRESS}
 # 检查四个环境变量是否都不为空
 if [[ -n "$MYSQL_USER_NAME" && -n "$MYSQL_USER_PASSWORD" && -n "$MYSQL_DATABASE" && -n "$MYSQL_IP_ADDRESS" ]]; then
    sed -i "s/\\\Filegator\\\Services\\\Auth\\\Adapters\\\JsonFile/\\\Filegator\\\Services\\\Auth\\\Adapters\\\Database/g" $FILE_PATH
-   sed -i "s/'file' => __DIR__\.'\/private\/users.json',/'driver' => 'mysqli',\n        'host' => 'localhost',\n        'username' => 'root',\n        'password' => 'password',\n        'database' => 'filegator',/g" $FILE_PATH
+   sed -i "s/'file' => __DIR__\.'\/private\/users.json',/'driver' => 'mysqli',\n                'host' => '$MYSQL_IP_ADDRESS',\n                'username' => '$MYSQL_USER_NAME',\n                'password' => '$MYSQL_USER_PASSWORD',\n                'database' => '$MYSQL_DATABASE',/g" $FILE_PATH
 fi
 
 # 启动apache
